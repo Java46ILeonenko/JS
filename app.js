b0d9768256806f0a91bfa1be32adeb81ab4cd809
IgorLeonenko <leonenko.i13i@gmail.com>
2022-01-16 17:07:33 +0200

    HW11

--- app.js
+++ app.js
@@ -1,42 +1,22 @@
-console.log("");
-
-console.log("a" + ("a" - "s") + "a" + "s");      //1
-console.log("");
-
-console.log(calculator(5,2,'/'));                //2
-function calculator(number1, number2, option) {
-    switch(option) {               
-        case '+':
-             result = number1 + number2;
-            break;
-    
-        case '-':
-             result = number1 - number2;
-            break;
-    
-        case '*':
-             result = number1 * number2;
-            break;
-    
-        case '/':
-             if(number2==0){
-                return  ("ERROR");
-             }
-             else{
-                result = number1 / number2;
-             }
-            break;
-        default : return "ERROR: enter operator from '+','-','*','/'";
+function encode(number,codingString){
+    let result= "";
+    if(duplicate(codingString)==true){
+        console.log(`ERROR: "Duplicate symbols"`);
     }
-    return "Result: " +result;
-}
-
-console.log("");
-console.log(a(5)(10, 3));                        //3
-function a(sum) {
-    return  function add (b, c) {
-        return "Result: " +sum * (b * c);
+    else{
+        do {
+            const digit= Math.trunc(number%codingString.length);
+            const symbol= getS(digit,codingString);
+            result= symbol+result;
+            number= Math.trunc(number/codingString.length);
+        }while(number>=1);
+        return result;
+    }
+    function getS(digit,codingString) {
+        return "" + codingString[digit];
     }
 }
-
-console.log("");
+function duplicate(codingString){
+    return new Set(codingString).size!=codingString.length;
+}
+console.log(encode(346464634,`+=-90`));
