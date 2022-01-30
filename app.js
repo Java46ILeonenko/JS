44b6439a429c7fc8d0d6316c025a6eb7c0a89ea1
IgorLeonenko <leonenko.i13i@gmail.com>
2022-01-30 18:26:05 +0200

    HW15

--- app.js
+++ app.js
@@ -1,50 +1,34 @@
 console.log("1:");
 
-let myForEach = function(array, callback) {  
-  for (let i = 0; i < array.length; i++) {
-    callback(array[i], i, array);
-  }
-}
-let myFilter = function(array, callback){
-  const res = [];
-  function forEachCall(n, i, a) {
-      if (callback(n, i, a) == true)
-      res.push(n);
-  }
-  myForEach(array, forEachCall);
+function countOccurrences(array) {
+  const res = {};
+  array.reduce((_cur, n) => res[n] === undefined ? res[n] = 1 : res[n]++, 0);
   return res;
 }
-const ex = [2,4,5,7,8,9,10];
-const array0 = myFilter(ex, (n, i, a) => a.length % 2 == 0 ? n % 2 == 0 : n % 2 == 1);
-const array1 = myFilter(ex, (n, i, a) => Math.pow(n, 2) - a.length > 0);
-console.log(`  1:`, array0, ` 2:`,array1);
+function displayOccurrences(array) {
+  const res = countOccurrences(array);
+  Object.entries(res).sort((e1, e2) =>
+  e2[1] - e1[1] === 0 ? e1[0].localeCompare(e2[0]) : e2[1] - e1[1]
+  ).forEach(e => console.log(`${e[0]} -> ${e[1]}`))
+}
+const a = ["lmn","d","d","lmn","a","lmn","a","bc"];
+console.log(`Word count = ${a.length}`);
+displayOccurrences(a);
 
 console.log("2:");
 
-function createPerson(id, name, address) {
-  return {id, name, address};
+function countBy(array, callbackFun) {
+  return countOccurrences(array.map((n, i, a) => callbackFun(n, i, a)));
 }
-function createAddress(city, street) {
-  return {city, street}
-}
-const persons = [
-  createPerson(123, "Vasya", createAddress("Rehovot","Parshani")),
-  createPerson(124, "Olya", createAddress("Rehovot","Pr.Plaut")),
-  createPerson(125, "Tolya", createAddress("Tel-Aviv","Dizengoff")),
-  createPerson(126, "Sara", createAddress('Lod', 'Sokolov'))
-]
-function getPersonsCity(persons, city) {
-  const res = [];
-  persons.forEach((p, i) => {
-  if (persons[i].address.city === city)
-  res.push(JSON.stringify(p))
-  });
-  return res;
-}
-const rehovot = getPersonsCity(persons, "Rehovot");
-console.log(`  Rehovot :`,rehovot);
+const a0 = [6.4, 7.3, 6.5, 6.9];
+const statistics0 = countBy(a0, element => Math.floor(element));
+console.log("   1:",statistics0,"\n");
 
-console.log("3:");
+const a1 = ['abcd', 'lmnr', 'ab', 'dddd'];
+const statistics1 = countBy(a1, element => element.length);
+console.log("   2:",statistics1,"\n");
 
-persons.sort((a, b) => a.address.city != "Rehovot"? -1 : 1);
-console.log(`  `,persons);
+const a2 = [{age: 25, id: 123, name: 'Vasya'},{age: 50, id: 123, name: 'Vasya'},
+{age: 25, id: 123, name: 'Vasya'},{age: 70, id: 123, name: 'Vasya'}];
+const statistics2 = countBy(a2, element => element.age);
+console.log("   3:",statistics2,"\n")
