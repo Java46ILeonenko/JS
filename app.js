4c518dd401db4b9e135c2156df5f7606841d7a70
IgorLeonenko <leonenko.i13i@gmail.com>
2022-02-02 19:46:44 +0200

    HW16

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
-console.log("1:");
+console.log(`\t1:`);
 
-function countOccurrences(array) {
-  const res = {};
-  array.reduce((_cur, n) => res[n] === undefined ? res[n] = 1 : res[n]++, 0);
-  return res;
+function Deferred(){
+    this.array0 = [];
+    this.result;
+  }
+  Deferred.prototype.then = function(e) {
+    this.array0.push(e);
+  }
+  Deferred.prototype.resolve = function(value) {
+    this.result = value;
+    for (let i = 0; i < this.array0.length; i++) {
+        this.result = this.array0[i](this.result);
+    }
+  }
+
+  const d = new Deferred();
+  d.then(function(result){ console.log('1', result); return 'a'; });
+  d.then(function(result){ console.log('2', result); return 'b'; });
+  d.then(function(result){ console.log('3', result); return 'c'; });
+  d.resolve('Hello');
+
+console.log(`\t2:`);
+
+function MyArray(value){
+    this.array1 = []; 
+    this.value = value;
 }
-function displayOccurrences(array) {
-  const res = countOccurrences(array);
-  Object.entries(res).sort((e1, e2) =>
-  e2[1] - e1[1] === 0 ? e1[0].localeCompare(e2[0]) : e2[1] - e1[1]
-  ).forEach(e => console.log(`${e[0]} -> ${e[1]}`))
+MyArray.prototype.get = function(i) {
+    return this.array1[i]? this.array1[i] : this.value;
 }
-const a = ["lmn","d","d","lmn","a","lmn","a","bc"];
-console.log(`Word count = ${a.length}`);
-displayOccurrences(a);
-
-console.log("2:");
-
-function countBy(array, callbackFun) {
-  return countOccurrences(array.map((n, i, a) => callbackFun(n, i, a)));
+MyArray.prototype.set = function(i,v) {
+    this.array1[i] = v;
 }
-const a0 = [6.4, 7.3, 6.5, 6.9];
-const statistics0 = countBy(a0, element => Math.floor(element));
-console.log("   1:",statistics0,"\n");
-
-const a1 = ['abcd', 'lmnr', 'ab', 'dddd'];
-const statistics1 = countBy(a1, element => element.length);
-console.log("   2:",statistics1,"\n");
-
-const a2 = [{age: 25, id: 123, name: 'Vasya'},{age: 50, id: 123, name: 'Vasya'},
-{age: 25, id: 123, name: 'Vasya'},{age: 70, id: 123, name: 'Vasya'}];
-const statistics2 = countBy(a2, element => element.age);
-console.log("   3:",statistics2,"\n")
+MyArray.prototype.setValue = function (v) {
+    this.value = v;
+    this.array1 = this.array1.map(n => n = v);
+}
+const myArray = new MyArray(10);
+console.log(myArray.get(100));
+myArray.set(100, 500);
+console.log(myArray.get(200));
+console.log(myArray.get(100));
+myArray.setValue(300);
+console.log(myArray.get(100));
+console.log(myArray.get(200));
